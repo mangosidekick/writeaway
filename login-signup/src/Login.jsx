9a2264da89f21c7ee1d0e063d74afbe05d9e8fbf
mangosidekick <pokeyp0kers3@gmail.com>
2024-11-13 20:10:41 +0800

    react app but im unsure about it HAHAHAHHA

--- login-signup/src/Login.jsx
+++ login-signup/src/Login.jsx
@@ -1,5 +1,25 @@
-export const Login = () => {
+import React, { useState } from "react";
+
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
     return(
-        <>Login</>
+        
+        <div className="auth-form-container">
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email">email</label>
+            <input value={email} type="email" placeholder="enter your email here" id="email" name="email"/>
+
+            <label htmlFor="password">password</label>
+            <input  value={pass} type="password" placeholder="enter password" id="password" name="password"/>
+            <button type="submit">Log In</button>
+        </form>
+        <button onClick={() => props.onFormSwitch('register')}>new to writeaway? sign up here!</button>
+        </div>
     );
 }
