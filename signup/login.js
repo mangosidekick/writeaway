92927453732b83cfd285e6afff365ef9d299c706
mangosidekick <pokeyp0kers3@gmail.com>
2024-11-15 10:10:17 +0800

    quick fix

--- signup/login.js
+++ signup/login.js
@@ -12,18 +12,3 @@ registerEyeBtn.addEventListener("click", () => {
         registerEyeBtn.classList.replace("fa-eye-slash", "fa-eye");
     }
 });
-
-const registerEyeBtn2 = document.querySelector(".forget-password-icon");
-const registerInputField2 = document.querySelector(".repeat-password");
-
-//register repeat password visibility
-registerEyeBtn2.addEventListener("click", () => {
-    if (registerInputField2.type === "password"){
-        registerInputField2.type = "text";
-        registerEyeBtn2.classList.replace("fa-eye", "fa-eye-slash");
-        console.log(registerEyeBtn2);
-    }else{
-        registerInputField2.type = "password";
-        registerEyeBtn2.classList.replace("fa-eye-slash", "fa-eye");
-    }
-});
